f2107c83899c9e455b43bb2a246d1cb5b9ac047a
Mit Donga <meetdonga5344@gmail.com>
2023-11-26 15:25:23 +0530

    Added /details route

--- index.js
+++ index.js
@@ -10,4 +10,8 @@ app.get('/health', (req, res) => {
 	res.status(200).json({ message: "Server is up and running.." })
 })
 
+app.get('/details', (req, res) => {
+	res.status(200).json({ message: "This is a demo project to set up CI pipeline using GitHub Actions" })
+})
+
 app.listen(port, () => { console.log('Express app running on port', port)});
