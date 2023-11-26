53611ec36ce08668f74dbdeb1f7164b0211e4089
Mit Donga <meetdonga5344@gmail.com>
2023-11-26 15:08:17 +0530

    Added CD using github actions

--- index.js
+++ index.js
@@ -6,7 +6,7 @@ app.get('/', (req, res) => {
 	res.status(200).json({ message: "Success.." })
 })
 
-app.get('/heath', (req, res) => {
+app.get('/health', (req, res) => {
 	res.status(200).json({ message: "Server is up and running.." })
 })
 
